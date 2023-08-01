48ad3d2de4ff67fdfba6f83fc99516dd340ce047
LexSheyn <tiadec@gmail.com>
2023-08-01 11:36:16 +0500

    Implemented event callback.

--- Project_0/source/main.cpp
+++ Project_0/source/main.cpp
@@ -9,7 +9,130 @@
 #include <iostream>
 #include <cstdint>
 
-// TO DO
+class ManipulatorController : public osgGA::GUIEventHandler
+{
+public:
+
+    ManipulatorController()
+    : m_currentIndex (0)
+    {}
+
+    void addTarget(osgEarth::MapNode* target)
+    {
+        const osg::observer_ptr<osgEarth::MapNode> targetPointer = target;
+
+        m_targets.push_back(targetPointer);
+    }
+
+    void removeTarget(uint32_t index)
+    {
+        m_targets.erase(m_targets.begin() + index);
+    }
+
+    void setTarget(uint32_t index, osgEarth::MapNode* target)
+    {
+        m_targets[index] = target;
+    }
+    
+    const osgEarth::MapNode* getTarget(uint32_t index) const
+    {
+        osg::ref_ptr<osgEarth::MapNode> target;
+
+        if (m_targets[index].lock(target))
+        {
+            return target.get();
+        }
+
+        return nullptr;
+    }
+
+    bool followTarget(uint32_t index)
+    {
+        osg::ref_ptr<osgEarth::MapNode> target;
+
+        if (m_targets[index].lock(target))
+        {
+            osg::ref_ptr<osgEarth::EarthManipulator> manipulator;
+
+            if (m_manipulator.lock(manipulator))
+            {
+                manipulator->setNode(target.get());
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void setManipulator(osgEarth::EarthManipulator* manipulator)
+    {
+        m_manipulator = manipulator;
+    }
+
+    // osgGA::GUIEventHandler.
+
+    virtual bool handle(const osgGA::GUIEventAdapter& eventAdapter, osgGA::GUIActionAdapter& actionAdapter) override
+    {
+        const osgGA::GUIEventAdapter::EventType eventType = eventAdapter.getEventType();
+
+        if (eventType == osgGA::GUIEventAdapter::KEYDOWN)
+        {
+            const int32_t key = eventAdapter.getKey();
+
+            if (!m_targets.empty())
+            {
+                switch (key)
+                {
+                    case 'q':
+                    case 'Q':
+                    {
+                        m_currentIndex--;
+
+                        if (m_currentIndex < m_targets.size())
+                        {
+                            const bool b_followed = this->followTarget(m_currentIndex);
+                        }
+                        else
+                        {
+                            m_currentIndex++;
+                        }
+
+                        break;
+                    }
+
+                    case 'e':
+                    case 'E':
+                    {
+                        m_currentIndex++;
+
+                        if (m_currentIndex < m_targets.size())
+                        {
+                            const bool b_followed = this->followTarget(m_currentIndex);
+                        }
+                        else
+                        {
+                            m_currentIndex--;
+                        }
+
+                        break;
+                    }
+
+                    default: break;
+                }
+            }
+        }
+    }
+
+protected:
+
+    virtual ~ManipulatorController() override
+    {}
+
+    std::vector<osg::observer_ptr<osgEarth::MapNode>> m_targets;
+    osg::observer_ptr<osgEarth::EarthManipulator>     m_manipulator;
+    uint32_t                                          m_currentIndex;
+};
 
 int32_t main(int32_t argc, char** argv)
 {
